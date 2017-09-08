1694c70ab21b55b6215cb3e3139d003eb29913a9
mike <paulrimiru@gmail.com>
2017-09-08 06:53:13 +0300

    (FIX) made chages to tests

--- test/admin_test.py
+++ test/admin_test.py
@@ -8,29 +8,28 @@ class AdminTest(unittest.TestCase):
     """class containing tests for admin.py methods"""
     def setUp(self):
         self.admin = Admin()
-        self.user = User("Mike", "Paul", "mikepaul@shoppilist.com", 123456789)
+        self.user = User("Mike", "Paul", "mikepaul@shoppilist.com", "123456789")
     def test_add_user(self):
         """test to add a user"""
-        myuser = User("Mike", "Paul", "mikepaul@shoppilist.com", 123456789)
-        
+        myuser = User("Mike", "Paul", "mikepaul@shoppilist.com", "123456789")
         self.admin.add_user(myuser)
 
         self.assertEqual(1, len(self.admin.get_all_users()))
     def test_remove_user(self):
         """test to remove a user"""
-        reception = User("Mike", "Paul", "mikepaul@shoppilist.com", 123456789)
+        reception = User("Mike", "Paul", "mikepaul@shoppilist.com", "123456789")
         self.admin.add_user(reception)
         self.assertEqual(1, len(self.admin.get_all_users()))
         self.admin.remove_user("mikepaul@shoppilist.com")
         self.assertEqual(0, len(self.admin.get_all_users()))
     def test_update_user(self):
         """test to determine if user is updated"""
-        myuser = User("Mike", "Paul", "mikepaul@shoppilist.com", 123456789)
+        myuser = User("Mike", "Paul", "mikepaul@shoppilist.com", "123456789")
         
         self.admin.add_user(myuser)
         self.assertEqual(1, len(self.admin.get_all_users()))
 
-        newuser = User("Mike", "Paul", "mikepaul@shoppilist.com", 123456789)
+        newuser = User("Mike", "Paul", "mikepaul@shoppilist.com", "123456789")
         self.admin.update_user(
             myuser.email, newuser)
 
